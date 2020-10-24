960925e42257c954c19a82ad9bf9d10b847c6eca
Mark Ito <marki@jlab.org>
2020-10-24 16:40:56 -0400

    Give user an option of what to do with a pre-exising container, quit, keep, or overwrite.

--- scripts/create_gluex_container.sh
+++ scripts/create_gluex_container.sh
@@ -6,9 +6,7 @@ show_help() {
 
 parse_command_line_options() {
     local OPTIND opt
-    echo debug: parsing
     while getopts "h?g:d:t:" opt; do
-	echo "before case"
 	case "$opt" in
 	    h|\?)
 		show_help
@@ -23,12 +21,32 @@ parse_command_line_options() {
 	esac
     done
     shift $((OPTIND-1))
-
     recipe=$1
-    echo "done parsing"
+}
+
+file_found_action() {
+    prompt_in=$1
+    file_in=$2
+    prompt="$prompt_in
+quit/keep/overwrite? "
+    while true; do
+	read -p "$prompt" qco
+	case $qco in
+            [Qq]* ) exit 4;;
+            [Cc]* ) result=keep; break;;
+	    [Oo]* ) rm -rf $file_in; result=build; break;;
+	    * ) echo "Please answer quit, keep, or overwrite";;
+	esac
+    done
 }
 
 parse_command_line_options "$@"
+
+if [ ! -f $recipe ]
+then
+    echo ERROR: recipe file $recipe not found
+    exit 2
+fi
 
 if [ -z "$container_meta_dir" ]
 then
@@ -41,34 +59,48 @@ raw_sandbox=$container_meta_dir/$dist_token
 gluex_sandbox=$container_meta_dir/gluex_$dist_token
 gluex_sif=$container_meta_dir/gluex_$dist_token.sif
 
+result=build
 if [ -d $raw_sandbox ]
 then
-    echo raw sandbox $raw_sandbox exists, exiting
-    exit 1
+    file_found_action "raw sandbox $raw_sandbox exists" $raw_sandbox
 fi
-echo building sandbox container $raw_sandbox according to $recipe
-singularity build --sandbox $raw_sandbox $recipe
+if [ $result == build ]
+then
+    echo INFO: building sandbox container $raw_sandbox according to $recipe
+    singularity build --sandbox $raw_sandbox $recipe
+fi
 
+if [ ! -f $gluex_prereqs_script ]
+then
+    echo ERROR: gluex software install script $gluex_prereqs_script not found
+    exit 3
+fi
+
+result=build
 if [ -d $gluex_sandbox ]
 then
-    echo gluex sandbox $gluex_sandbox exists, exiting
-    exit 1
+    file_found_action "gluex sandbox $gluex_sandbox exists" $gluex_sandbox
 fi
-echo copying $raw_sandbox to $gluex_sandbox
-cp -pr $raw_sandbox $gluex_sandbox
-echo create /gluex_install mount point in $gluex_sandbox
-singularity exec --writable $gluex_sandbox mkdir /gluex_install
-echo install gluex software into $gluex_sandbox using $gluex_prereqs_script
-gpbase=`basename $gluex_prereqs_script`
-gpdir=`dirname $gluex_prereqs_script`
-singularity exec --bind $gpdir:/gluex_install --writable $gluex_sandbox /gluex_install/$gpbase
-
-if [ -d $gluex_sif ]
+if [ $result == build ]
 then
-    echo gluex simg $gluex_sif exists, exiting
-    exit 1
+   echo INFO: copying $raw_sandbox to $gluex_sandbox
+   cp -pr $raw_sandbox $gluex_sandbox
+   echo INFO: creating /gluex_install mount point in $gluex_sandbox
+   singularity exec --writable $gluex_sandbox mkdir /gluex_install
+   echo INFO: installing gluex software into $gluex_sandbox using $gluex_prereqs_script
+   gpbase=`basename $gluex_prereqs_script`
+   gpdir=`dirname $gluex_prereqs_script`
+   singularity exec --bind $gpdir:/gluex_install --writable $gluex_sandbox /gluex_install/$gpbase
 fi
-echo build squashfs sandbox $gluex_sif from $gluex_sandbox
-singularity build $gluex_sif $gluex_sandbox
 
+result=build
+if [ -f $gluex_sif ]
+then
+    file_found_action "gluex simg $gluex_sif exists" $gluex_sif
+fi
+if [ $result == build ]
+then
+    echo INFO: build squashfs sandbox $gluex_sif from $gluex_sandbox
+    singularity build $gluex_sif $gluex_sandbox
+fi
 
